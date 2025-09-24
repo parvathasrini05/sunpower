d85f37c800df0e16794905a0524fa4b0493dc157
haristha14jan6 <haristha1983@gamil.com>
2025-09-24 20:25:56 +0530

    hi

---
+++ src/main/java/com/example/demo/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.batteryshop.services;
+
+import com.batteryshop.models.Product;
+import com.batteryshop.repositories.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product addProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public Product getProductById(Long id) {
+        return productRepository.findById(id).orElseThrow();
+    }
+}
